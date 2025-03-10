560d9b83c0747861b12fc692cb8b99d455fed93a
idossha <idochaber@gmail.com>
2025-03-09 19:35:56 -0500

    key mapping robusteness

--- ftplugin/matlab.lua
+++ ftplugin/matlab.lua
@@ -8,13 +8,83 @@ vim.b.did_ftplugin_matlab_nvim = true
 
 local config = require('matlab.config')
 
+-- Ensure mappings are defined even if there's a configuration issue
+local function get_safe_mappings()
+  -- Check if mappings exist in config
+  local user_mappings = config.get('mappings')
+  
+  -- Default mappings as fallback
+  local default_mappings = {
+    prefix = '<Leader>m',
+    run = 'r',
+    run_cell = 'c',
+    run_to_cell = 't',
+    breakpoint = 'b',
+    clear_breakpoint = 'd',
+    clear_breakpoints = 'D',
+    doc = 'h',
+    toggle_workspace = 'w',
+    show_workspace = 'W',
+    clear_workspace = 'x',
+    save_workspace = 's',
+    load_workspace = 'l',
+    toggle_cell_fold = 'f',
+    toggle_all_cell_folds = 'F',
+  }
+  
+  -- If user_mappings doesn't exist or is malformed, use defaults
+  if type(user_mappings) ~= 'table' then
+    vim.notify("MATLAB: Using default mappings (config.mappings is not a table)", vim.log.levels.WARN)
+    return default_mappings
+  end
+  
+  -- Merge user_mappings with defaults
+  for k, v in pairs(default_mappings) do
+    if user_mappings[k] == nil then
+      user_mappings[k] = v
+    end
+  end
+  
+  return user_mappings
+end
+
+-- Print debug info
+local function debug_info(message)
+  if config.get('debug') then
+    vim.notify("MATLAB Debug: " .. message, vim.log.levels.INFO)
+  end
+end
+
 -- Set up key mappings if enabled in config
 if config.get('default_mappings') then
-  local mappings = config.get('mappings')
-  local prefix = mappings.prefix or '<Leader>m'
+  local mappings = get_safe_mappings()
+  local prefix = mappings.prefix
   
-  -- Run commands
-  vim.keymap.set('n', prefix .. mappings.run, '<Cmd>MatlabRun<CR>', { buffer = true, desc = 'Run MATLAB script' })
+  -- Log mapping information
+  debug_info("Setting up MATLAB mappings with prefix: " .. prefix)
+  
+  -- Directly set some important mappings with space for testing
+  if vim.g.mapleader == " " then
+    -- Test explicit space mappings
+    local space_mappings = {
+      [' mr'] = '<Cmd>MatlabRun<CR>',
+      [' mc'] = '<Cmd>MatlabRunCell<CR>',
+      [' mb'] = '<Cmd>MatlabBreakpoint<CR>',
+      [' mh'] = '<Cmd>MatlabDoc<CR>',
+      [' mw'] = '<Cmd>MatlabToggleWorkspace<CR>'
+    }
+    
+    for lhs, rhs in pairs(space_mappings) do
+      vim.keymap.set('n', lhs, rhs, { buffer = true, desc = 'MATLAB command' })
+      debug_info("Set explicit space mapping: " .. lhs)
+    end
+  end
+  
+  -- Run commands - with more debugging
+  local map_run = prefix .. mappings.run
+  debug_info("Setting mapping: " .. map_run)
+  vim.keymap.set('n', map_run, '<Cmd>MatlabRun<CR>', { buffer = true, desc = 'Run MATLAB script' })
+  
   vim.keymap.set('n', prefix .. mappings.run_cell, '<Cmd>MatlabRunCell<CR>', { buffer = true, desc = 'Run current MATLAB cell' })
   vim.keymap.set('n', prefix .. mappings.run_to_cell, '<Cmd>MatlabRunToCell<CR>', { buffer = true, desc = 'Run to current MATLAB cell' })
 
@@ -36,4 +106,19 @@ if config.get('default_mappings') then
   -- Cell folding
   vim.keymap.set('n', prefix .. mappings.toggle_cell_fold, '<Cmd>MatlabToggleCellFold<CR>', { buffer = true, desc = 'Toggle current MATLAB cell fold' })
   vim.keymap.set('n', prefix .. mappings.toggle_all_cell_folds, '<Cmd>MatlabToggleAllCellFolds<CR>', { buffer = true, desc = 'Toggle all MATLAB cell folds' })
+  
+  -- Additional mapping for commonly used functions using just the prefix
+  vim.keymap.set('n', prefix, '<Cmd>MatlabRun<CR>', { buffer = true, desc = 'Run MATLAB script (shortcut)' })
+  
+  -- Add command to view key mappings
+  vim.api.nvim_buf_create_user_command(0, 'MatlabKeymaps', function()
+    local lines = {"MATLAB key mappings:"}
+    table.insert(lines, "- " .. prefix .. mappings.run .. " : Run MATLAB script")
+    table.insert(lines, "- " .. prefix .. mappings.run_cell .. " : Run current MATLAB cell")
+    table.insert(lines, "- " .. prefix .. mappings.run_to_cell .. " : Run to current MATLAB cell")
+    table.insert(lines, "- " .. prefix .. mappings.breakpoint .. " : Set breakpoint")
+    table.insert(lines, "- " .. prefix .. mappings.clear_breakpoint .. " : Clear breakpoint")
+    table.insert(lines, "- " .. prefix .. mappings.doc .. " : Show documentation")
+    vim.notify(table.concat(lines, "\n"), vim.log.levels.INFO)
+  end, {})
 end
--- plugin/matlab.vim
+++ plugin/matlab.vim
@@ -3,5 +3,37 @@ if exists('g:loaded_matlab_nvim')
 endif
 let g:loaded_matlab_nvim = 1
 
+" Ensure proper filetype detection for MATLAB files
+augroup matlab_filetype_detection
+  autocmd!
+  autocmd BufRead,BufNewFile *.m set filetype=matlab
+augroup END
+
+" Enable debugging if set in init.vim/init.lua
+if exists('g:matlab_debug') && g:matlab_debug
+  echom "MATLAB.nvim: Debug mode enabled"
+endif
+
 " Load Lua functionality
 lua require('matlab').setup({})
+
+" Add a command to debug keymap issues
+command! -nargs=0 MatlabDebugKeymaps call s:DebugKeymaps()
+
+function! s:DebugKeymaps()
+  echo "MATLAB Keymap Debug Info:"
+  echo "Leader key: '" . mapleader . "'"
+  echo "Current filetype: " . &filetype
+  echo "Checking for matlab.lua plugin file..."
+  let ftplugin_path = findfile('ftplugin/matlab.lua', &rtp)
+  if empty(ftplugin_path)
+    echo "  - Not found!"
+  else
+    echo "  - Found at: " . ftplugin_path
+  endif
+  
+  echo "\nTrying to set direct keymaps..."
+  nnoremap <buffer> <space>mr :echo "Direct space-mr mapping works!"<CR>
+  nnoremap <buffer> <Leader>mr :echo "Direct leader-mr mapping works!"<CR>
+  echo "Direct keymaps set for testing. Try pressing <space>mr or <Leader>mr"
+endfunction
